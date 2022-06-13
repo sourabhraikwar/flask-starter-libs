b69151161995549f2a0cca34e94eb660294b0274
sourabhraikwar <sourabhraikwar11111@gmail.com>
2022-06-13 23:54:17 +0530

    [#0] FEAT: heroku deployment ready

---
+++ main/core/routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+core_bp = Blueprint('core_bp', __name__)
+
+@core_bp.route('/')
+def index():
+    return "Hello Everyone"
--- main/settings.py
+++ main/settings.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask
-
+from main.core.routes import core_bp
 
 def create_app(config=os.environ.get('', 'main.config.DevelopmentConfig')):
     app = Flask(__name__)
     app.config.from_object(config)
     app.config['DEBUG'] = True
+
+    # blueprint registration
+    app.register_blueprint(core_bp)
     return app
