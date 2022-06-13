2f8c9b47ad4f1f966ed2727ff91b2eb33402686f
Sourabh Raikwar <sourabhraikwar11111@gmail.com>
2022-06-13 23:57:59 +0530

    [#0] FEAT:  one comment

--- main/settings.py
+++ main/settings.py
@@ -5,6 +5,7 @@ from main.core.routes import core_bp
 def create_app(config=os.environ.get('', 'main.config.DevelopmentConfig')):
     app = Flask(__name__)
     app.config.from_object(config)
+    # app debug enabled by default
     app.config['DEBUG'] = True
 
     # blueprint registration
